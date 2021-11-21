e1748b0579b219a82588d1f818810be0ae74d281
mnty4 <zenikyn@gmail.com>
2021-11-21 16:53:54 +1100

    fixed message text not wrapping

--- client/src/components/Message/Message.js
+++ client/src/components/Message/Message.js
@@ -11,14 +11,17 @@ const Message = ({ userData }) => (
       alignItems: "flex-start",
       margin: "10px 0 10px 0",
       padding: "10px",
-      justifyContent: "flex-start",
+      width: "18em",
+      textAlign: "left",
       gap: "10px",
     }}
   >
     <p style={{ margin: "0" }}>
       {userData.username} - {userData.time}
     </p>
-    <p style={{ margin: "0" }}>{userData.message}</p>
+    <p style={{ margin: "0", fontSize: "16px", fontWeight: "bold" }}>
+      {userData.message}
+    </p>
   </div>
 );
 
