9a2c47037f85846147c3133d6600ec22353b37ee
mnty4 <zenikyn@gmail.com>
2021-11-24 20:49:58 +1100

    added new functionality for keeping track of occupied and recently left rooms

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,27 @@ const io = new Server(server, {
     methods: ["GET", "POST"],
   },
 });
+const rooms = [];
 
+const addToRoom = (room) => {
+  const roomInfo = rooms.find((r) => r[0] === room);
+  roomInfo ? ++roomInfo[1] : rooms.push([room, 1]);
+  console.log(rooms);
+};
+const removeFromRoom = (room) => {
+  const roomInfo = rooms.find((r) => r[0] === room);
+  if (roomInfo) --roomInfo[1];
+  console.log(rooms);
+};
+
+// const addUserToRoom = (room) => {
+//   if (rooms.find( => room === )) rooms.set(room, []);
+//   rooms.get(room).push(socket.id);
+// };
+// const removeUserFromRoom = (room) => {
+//   if (rooms.get(room).length <= 1) rooms.delete(room);
+//   else rooms.get(room).filter((id) => id !== socket.id);
+// };
 // TODO: view rooms in use, number of participants, number of participants reduces on disconnect
 
 io.on("connection", (socket) => {
@@ -25,14 +45,22 @@ io.on("connection", (socket) => {
   socket.on("join_room", (room) => {
     socket.join(room);
     console.log(`User with ID: ${socket.id} has joined ${room}`);
+    addToRoom(room);
   });
   socket.on("send_message", (messageData) => {
     console.log(messageData);
     socket.to(messageData.room).emit("receive_message", messageData);
+    // console.log(io.sockets.adapter.rooms);
   });
   socket.on("leave_room", (room) => {
     console.log(`User with ID: ${socket.id} has left ${room}`);
+    removeFromRoom(room);
     socket.leave(room);
+  });
+  socket.on("refresh_rooms", () => {
+    io.to(socket.id).emit("receive_refreshed_rooms", rooms);
+    console.log(rooms);
+    console.log(JSON.stringify(rooms));
   });
 
   socket.on("disconnect", () => {
